6798240320aeba750396963ce84a32f46ca29f03
Lisa <lisa.exz@gmail.com>
2020-01-16 19:16:02 +0200

    step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,4 +6,10 @@ import { version, description } from '../../package.json';
 program
   .version(version)
   .description(description)
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig);
+    console.log(secondConfig);
+  })
   .parse(process.argv);
